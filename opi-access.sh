14fa08993552958b46bd8b5941520b3d136de3fd
PA Nilsson <pa@openproducts.se>
2014-06-23 10:20:01 +0200

    Initil files for opi-access, port opener only.

---
+++ opi-access.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source /etc/opi/opi-access.conf
+date >> /tmp/opi-access.log
+
+ipaddr=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+
+for port in "${!ports[@]}"
+do   
+  if [ ${ports[$port]} == "yes" ]; then
+	echo "Opening port $port"
+	/usr/bin/upnpc -a $ipaddr $port $port tcp &> /dev/null
+  else
+	if [ ! -z "$1" ]; then
+		if [ $1 == "force" ]; then
+			echo "Closing port $port"
+			/usr/bin/upnpc -d $port tcp &>/dev/null
+		fi
+	fi
+  fi
+done
