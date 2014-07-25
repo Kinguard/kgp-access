04974eed7345d81e7c0d17cb2078bee011aa4ff2
PA Nilsson <pa@openproducts.se>
2014-07-25 09:44:01 +0200

    Added support to update by serial

--- dns_update.py
+++ dns_update.py
@@ -23,6 +23,8 @@ SUCCESS		= 1
 WAIT		= 2
 REQUEST_KEY	= 3
 
+DOMAIN = "op-i.me"
+
 def sendsignedchallenge(conn, unit_id, fp_pkey, challenge):
 	fh_pkey = open(fp_pkey,'r')
 	pkey_data=fh_pkey.read()
@@ -142,17 +144,67 @@ def add_section_header(properties_file, header_name):
 	for line in properties_file:
 		yield line
 
+def update_by_serial(conn):
+	import re
+	eeprom = "/sys/bus/i2c/devices/0-0050/eeprom"
+	try:
+		ee = open(eeprom,'rb')
+		serial = ee.read(16)[4:11].decode(encoding='UTF-8') # read first not interesting bytes
+		'''
+		print("Serial: ")
+		print(serial)
+		if 'A335' in serial:
+			print("A335 found")
+		'''
+		try:
+			serial = ee.read(12).decode(encoding='UTF-8')
+			#print("Serial: ")
+			#print(serial)
+			data = {}
+			data['fqdn'] = serial+"."+DOMAIN
+			params = urllib.parse.urlencode( data, doseq=True )
+			headers = {"Content-type": "application/x-www-form-urlencoded"}
+			path = urllib.parse.quote(AUTH_PATH + DNS_FILE)
 
+			conn.request("POST", path, params, headers)
+
+			r = conn.getresponse()
+			data = r.read()
+
+			if r.status not in ( 200, 403):
+				print("Wrong status %d"%r.status)
+				return False
+
+			rp = json.loads( data.decode('utf-8') )
+
+			if r.status == 200:
+				print("Got '200 OK'")
+				return True
+			if r.status == 403:
+				print("Failed to update")
+				return False
+
+		except Exception as e:
+			print("Unable to send request")
+			print(e)
+
+		ee.close()
+
+	except Exception as e:
+		print("Error reading eeprom")
+		print(e)
+
+	sys.exit(1)
 
 ### -------------- MAIN ---------------
 if __name__=='__main__':
 
+	dns_by_serial = False
 	try:
 		fh_sysconf = open(SYSINFO, encoding="utf_8")
 	except Exception as e:
 		print("Error opening SYSINFO file: "+SYSINFO)
 		print(e)
-		sys.exit(1)
 
 	sysconf = configparser.ConfigParser()
 	# There are no sections in our ini files, so add one on the fly.
@@ -162,50 +214,53 @@ if __name__=='__main__':
 			print("Missing parameters in sysinfo")
 			sys.exit(1)
 		sysinfo = sysconf['sysinfo']
-		if 'unit_id' not in sysinfo:
-			print("Missing unit_id in sysinfo")
-			sys.exit(1)
-		unit_id = sysinfo['unit_id'].strip('"')
 		if 'capath' not in sysinfo:
 			print("Missing capath in sysinfo")
 			sys.exit(1)
 		cafile = sysinfo['capath'].strip('"')
-		if 'dns_key' not in sysinfo:
-			print("Missing dns_key in sysinfo")
-			sys.exit(1)
-		fp_pkey = sysinfo['dns_key'].strip('"')
+		if 'unit_id' not in sysinfo:
+			# update dns by using serialnumber in flash
+			print("Missing unit_id in sysinfo, using serialnumber")
+			dns_by_serial = True
+		else:
+			unit_id = sysinfo['unit_id'].strip('"')
+			if not dns_by_serial:
+				if 'dns_key' not in sysinfo:
+					print("Missing dns_key in sysinfo")
+					sys.exit(1)
+					fp_pkey = sysinfo['dns_key'].strip('"')
 
 	except Exception as e:
 		print("Error parsing sysconfig")
 		print(e)
 		sys.exit(1)
 
+		if not dns_by_serial:
+			try:
+				fh_accessconf = open(ACCESSINFO, encoding="utf_8")
+			except Exception as e:
+				print("Error opening ACCESSINFO file: "+ACCESSINFO)
+				print(e)
+				sys.exit(1)
 
-	try:
-		fh_accessconf = open(ACCESSINFO, encoding="utf_8")
-	except Exception as e:
-		print("Error opening ACCESSINFO file: "+ACCESSINFO)
-		print(e)
-		sys.exit(1)
-
-	accessconf = configparser.ConfigParser()
-	# There are no sections in our ini files, so add one on the fly.
-	try:
-		accessconf.read_file(add_section_header(fh_accessconf, 'accessinfo'), source=ACCESSINFO)
-		if 'accessinfo' not in accessconf:
-			print("Missing parameters in accessinfo")
-			sys.exit(1)
-		accessinfo = accessconf['accessinfo']
-		if 'dyndns' not in accessinfo:
-			print("Missing dyndns parameters in accessinfo")
-			sys.exit(1)
-		if accessinfo['dyndns'].strip('"') != "yes":
-			print("DynDNS service not enabled.")
-			sys.exit(0)
-	except Exception as e:
-		print("Error parsing access config")
-		print(e)
-		sys.exit(1)
+			accessconf = configparser.ConfigParser()
+			# There are no sections in our ini files, so add one on the fly.
+			try:
+				accessconf.read_file(add_section_header(fh_accessconf, 'accessinfo'), source=ACCESSINFO)
+				if 'accessinfo' not in accessconf:
+					print("Missing parameters in accessinfo")
+					sys.exit(1)
+				accessinfo = accessconf['accessinfo']
+				if 'dyndns' not in accessinfo:
+					print("Missing dyndns parameters in accessinfo")
+					sys.exit(1)
+				if accessinfo['dyndns'].strip('"') != "yes":
+					print("DynDNS service not enabled.")
+					sys.exit(0)
+			except Exception as e:
+				print("Error parsing access config")
+				print(e)
+				sys.exit(1)
 
 
 	#print(unit_id)
@@ -228,11 +283,20 @@ if __name__=='__main__':
 
 		conn = http.client.HTTPSConnection(AUTH_SERVER, 443, context=ctx)
 
-		response = authenticate(conn, unit_id, fp_pkey)
+		if dns_by_serial:
+			response = update_by_serial(conn)
+
+		else:
+			response = authenticate(conn, unit_id, fp_pkey)
+			if response[0] == 200:
+				token = response[1]
+				if sendDNSupdate(conn,unit_id,token):
+					sys.exit(0)
+
+			sys.exit(1)
+
 	except http.client.HTTPException as e:
 		print(e)
-	token = response[1]
-	sendDNSupdate(conn,unit_id,token)
 
 
 
