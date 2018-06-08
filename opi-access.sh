7d3c2fd63bd92523b2452d32e0dc29edd8b8f23a
PA Nilsson <pa@openproducts.se>
2018-06-08 14:15:57 +0200

    Reading ports from sysconfig via libopi.

--- opi-access.sh
+++ opi-access.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source /etc/opi/opi-access.conf
 
 if /usr/bin/upnpc -s | grep -qs "Found valid IGD" ; then
 	echo "Valid IGD found"
@@ -7,20 +6,17 @@ else
 	echo "No valid IGD found"
 	exit 0
 fi
-ipaddr=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+ipaddr=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{print $1}'`
 
-for port in "${!ports[@]}"
+ports=$(kgp-sysinfo -p -c upnp -k forwardports)
+if [[ $? -ne 0 ]]; then
+	echo "Unable to get ports to forward."
+	exit 1
+fi
+
+for port in $ports
 do   
-  if [ ${ports[$port]} == "yes" ]; then
 	echo "Opening port $port"
 	/usr/bin/upnpc -a $ipaddr $port $port tcp &> /dev/null
-  else
-	if [ ! -z "$1" ]; then
-		if [ $1 == "force" ]; then
-			echo "Closing port $port"
-			/usr/bin/upnpc -d $port tcp &>/dev/null
-		fi
-	fi
-  fi
 done
 exit 0
